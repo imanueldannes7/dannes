9ae17de6045429c99b3b1a46e12f777aa1df1f33
imanueldannes7 <96125138+imanueldannes7@users.noreply.github.com>
2021-12-17 07:31:13 +0700

    Update streamlitbeta.py

--- streamlitbeta.py
+++ streamlitbeta.py
@@ -7,12 +7,12 @@ import streamlit as st
 
 #COBA DISPLAY DATA NEGARA DULU YG BISA DI INPUT SEBELOM INPUT NAMA NEGARANYA
 
-f = open("D:\\COOLYEAH\\Prokom\\UAS\\kode_negara_lengkap.json")
+f = open("kode_negara_lengkap.json")
 file_json = json.load(f)
 df_xsl = pd.read_excel(
-    "D:\\COOLYEAH\\Prokom\\UAS\\produksiminyakmentah.xlsx")
+    "produksiminyakmentah.xlsx")
 df_csv = pd.read_csv(
-    "D:\\COOLYEAH\\Prokom\\produksi_minyak_mentah.csv")
+    "produksi_minyak_mentah.csv")
 df_json = pd.DataFrame.from_dict(file_json, orient='columns')
 df_xsl['country_code'] = df_csv['kode_negara']
 df_xsl['Year'] = df_csv['tahun']
